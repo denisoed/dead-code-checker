b50311bff2ff24f4312732259bd8351ac060f957
Denisoed <denisoed@gmal.com>
2024-05-28 16:56:54 +0600

    Created class from main code

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+export const IGNORED_FUNCTIONS = [
+  'if',
+  'for',
+  'while',
+  'catch',
+  'switch',
+  'case',
+  'const',
+  'let',
+  'var',
+  'function',
+  'mounted',
+  'unmounted',
+  'created',
+  'updated',
+  'beforeMount',
+  'beforeUpdate',
+  'beforeDestroy',
+  'destroyed',
+  'setup'
+];
+export const DEFAULT_EXTENSIONS = ['.js', '.jsx', '.ts', '.tsx', '.vue'];
